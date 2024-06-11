0e0126cc4d8d936057374441865fb2b1e4fbb763
charlsjon <charlsjonangeloerrazo@gmail.com>
2024-06-11 18:38:37 +0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     let readMore = description.querySelector(".read-more");
     let content = description.querySelector(".content");
 
-    // Check if content exceeds 3 lines
     if (content.scrollHeight > content.clientHeight) {
       readMore.style.display = 'block';
     }
@@ -24,45 +23,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const input = document.getElementById("credit-card-input");
 
-// Function to format number
 const formatNumber = (number) => number.split("").reduce((acc, char, index) => {
     if (index !== 0 && index % 4 === 0) acc += " ";
     return acc + char;
 }, "");
 
-// Function to handle input and format number
 input.addEventListener("input", () => {
     input.value = formatNumber(input.value.replace(/\D/g, "")); // Replace non-numeric characters
 });
 
-// Prevent non-numeric input
 input.addEventListener("keydown", (event) => {
     const key = event.key;
     if (!/^\d$/.test(key) && !["Backspace", "ArrowLeft", "ArrowRight", "Delete"].includes(key)) {
-        event.preventDefault(); // Prevent non-numeric input
+        event.preventDefault();
     }
 });
 
-// Handle paste event to allow only numbers
 input.addEventListener("paste", (event) => {
     event.preventDefault();
-    const pastedData = event.clipboardData.getData("text").replace(/\D/g, ""); // Get only numbers from paste
-    document.execCommand("insertText", false, pastedData); // Insert only numeric data
+    const pastedData = event.clipboardData.getData("text").replace(/\D/g, "");
+    document.execCommand("insertText", false, pastedData);
 });
 
 const cvvInput = document.getElementById('cvv');
 
-// Allow only numerical input and limit length
 cvvInput.addEventListener('input', function (event) {
-    const value = this.value.replace(/\D/g, ''); // Remove non-numeric characters
-    if (value.length <= 4) { // CVV typically has a maximum of 4 digits
+    const value = this.value.replace(/\D/g, ''); 
+    if (value.length <= 4) {
         this.value = value;
     } else {
         this.value = value.slice(0, 4);
     }
 });
 
-// Prevent non-numeric input
 cvvInput.addEventListener('keydown', function (event) {
     const key = event.key;
     if (!/^\d$/.test(key) && !['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete'].includes(key)) {
@@ -70,9 +63,36 @@ cvvInput.addEventListener('keydown', function (event) {
     }
 });
 
-// Handle paste event to allow only numbers
 cvvInput.addEventListener('paste', function (event) {
     event.preventDefault();
     const pastedData = event.clipboardData.getData('text').replace(/\D/g, ''); // Get only numbers from paste
     document.execCommand('insertText', false, pastedData.slice(0, 4)); // Insert only numeric data up to 4 digits
 });
+
+let plans = document.getElementsByClassName('plan');
+
+for (let i = 0; i < plans.length; i++) {
+  const plan = plans[i];
+  plan.addEventListener('click', handlePlanClick)
+}
+
+function handlePlanClick(event) {
+  for (let i = 0; i < plans.length; i++) {
+    const plan = plans[i];
+    plan.classList.remove('select');
+    plan.classList.remove('bg-tealgreen');
+
+    event.currentTarget.classList.add('select');
+    event.currentTarget.classList.add('bg-tealgreen');
+    const paymentInfo = document.getElementById('payment-info');
+    const subscriptionPlan = document.getElementById('subscription-plan');
+
+    if(plan.classList.contains('select')) {
+      paymentInfo.style.width = "240px"
+      subscriptionPlan.classList.remove('rounded-lg');
+      subscriptionPlan.classList.add('rounded-l-lg');
+    }
+  }
+}
+
+
