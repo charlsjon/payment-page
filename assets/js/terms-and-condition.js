6940133e3556226f014df719b888d2758fe16305
charlsjon <charlsjonangeloerrazo@gmail.com>
2024-06-15 15:46:51 +0800

    updates

--- assets/js/terms-and-condition.js
+++ assets/js/terms-and-condition.js
@@ -5,5 +5,18 @@ scrollableElement.addEventListener('scroll', () => {
     clearTimeout(scrollableElement.scrollTimeout);
     scrollableElement.scrollTimeout = setTimeout(() => {
         scrollableElement.classList.remove('scroll-active');
-    }, 300); // Time in ms after scrolling stops to remove the class
+    }, 300);
 });
+
+const termsCondition = document.getElementById('accept-terms-condition');
+const continueBtn = document.getElementById('continue-btn');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  termsCondition.addEventListener('change', ()=> {
+    if(termsCondition.checked != true) {
+      continueBtn.setAttribute("disabled", "disabled")
+    } else {
+      continueBtn.removeAttribute("disabled")
+    }
+  })
+})
